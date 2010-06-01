6fb0b5ed7d97fc4d7d0d52bf16e5a8d0ec2e72e0
darrencauthon <darren@cauthon.com>
2010-06-01 10:25:03 -0500

    Implement the laziness blade.

--- src/MvcTurbine.Laziness/LazinessBlade.cs
+++ src/MvcTurbine.Laziness/LazinessBlade.cs
@@ -7,64 +7,15 @@ namespace MvcTurbine.Laziness
     {
         public override void Spin(IRotorContext context)
         {
-            //SetupUnity(context);
-
-            //SetupStructureMap(context);
-
-            //SetupNinject(context);
-
-            //SetupWindsor(context);
+            var serviceLocator = context.ServiceLocator;
+            serviceLocator.ResolveServices<ILazySetup>()
+                .ForEach(x =>
+                             {
+                                 if (x.CanSetup(serviceLocator))
+                                     x.Setup(serviceLocator);
+                             });
         }
 
-        //private void SetupWindsor(IRotorContext context)
-        //{
-        //    var serviceLocator = context.ServiceLocator as WindsorServiceLocator;
-        //    if (serviceLocator != null)
-        //    {
-        //        var container = serviceLocator.Container;
-        //        container.AddComponent("MvcTurbine.Laziness.ILazy<T>", typeof (ILazy<>), typeof (Lazy<>));
-        //    }
-        //}
-
-        //private void SetupNinject(IRotorContext context)
-        //{
-        //    var serviceLocator = context.ServiceLocator as NinjectServiceLocator;
-        //    if (serviceLocator != null)
-        //    {
-        //        var kernel = serviceLocator.Container;
-
-        //        kernel.Bind(typeof (ILazy<>)).ToMethod(ctx =>
-        //                                               (ctx.Kernel.Get(typeof (LazyProvider<>).MakeGenericType(ctx.GenericArguments)) as IProvider).Create(ctx));
-        //    }
-        //}
-
-        //public class LazyProvider<T> : Provider<ILazy<T>> where T : class
-        //{
-        //    protected override ILazy<T> CreateInstance(IContext context)
-        //    {
-        //        return new NinjectLazy<T>(() => context.Kernel.Get<T>());
-        //    }
-        //}
-
-        //private void SetupStructureMap(IRotorContext context)
-        //{
-        //    var serviceLocator = context.ServiceLocator as StructureMapServiceLocator;
-        //    if (serviceLocator != null)
-        //    {
-        //        var container = serviceLocator.Container;
-        //        container.Configure(x => { x.ForRequestedType(typeof (ILazy<>)).TheDefaultIsConcreteType(typeof (Lazy<>)); });
-        //    }
-        //}
-
-        //private void SetupUnity(IRotorContext context)
-        //{
-        //    var serviceLocator = context.ServiceLocator as UnityServiceLocator;
-        //    if (serviceLocator != null)
-        //    {
-        //        var container = serviceLocator.Container;
-        //        container.RegisterType(typeof (ILazy<>), typeof (Lazy<>));
-        //    }
-        //}
         public void AddRegistrations(AutoRegistrationList registrationList)
         {
             registrationList.Add(
