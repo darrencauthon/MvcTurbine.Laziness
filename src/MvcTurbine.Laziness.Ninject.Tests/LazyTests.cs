8073d4a5be9b51e38d199d57b1189f304ba785d3
darrencauthon <darren@cauthon.com>
2010-06-01 06:58:41 -0500

    Setup Ninject lazy testing.

--- src/MvcTurbine.Laziness.Ninject.Tests/LazyTests.cs
+++ src/MvcTurbine.Laziness.Ninject.Tests/LazyTests.cs
@@ -1,158 +1,129 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using MvcTurbine.ComponentModel;
-//using NUnit.Framework;
-//using Should;
+﻿using System;
+using System.Collections.Generic;
+using MvcTurbine.ComponentModel;
+using NUnit.Framework;
+using Should;
 
-//namespace MvcTurbine.Laziness.Ninject.Tests
-//{
-//    [TestFixture]
-//    public class NinjectLazyTests
-//    {
-//        [Test]
-//        public void Throws_an_invalid_operation_exception_when_called_without_setting_the_service_locator()
-//        {
-//            var correctExceptionThrown = false;
-//            try
-//            {
-//                var lazy = new Lazy<TestClass>(null);
-//            }
-//            catch (InvalidOperationException exception)
-//            {
-//                if (exception.Message == "Must set the service locator.")
-//                    correctExceptionThrown = true;
-//            }
+namespace MvcTurbine.Laziness.Ninject.Tests
+{
+    [TestFixture]
+    public class NinjectLazyTests
+    {
+        [Test]
+        public void Returns_the_value_from_the_creator_method()
+        {
+            var expected = new TestClass();
+            var lazy = new NinjectLazy<TestClass>(() => expected);
+            lazy.Value.ShouldBeSameAs(expected);
+        }
+    }
 
-//            correctExceptionThrown.ShouldBeTrue();
-//        }
+    public class TestClass
+    {
+    }
 
-//        [Test]
-//        public void Does_not_throw_an_invalid_operation_exception_if_the_service_locator_is_set()
-//        {
-//            var lazy = new Lazy<TestClass>(new TestServiceLocator());
-//            var value = lazy.Value;
-//        }
+    public class TestServiceLocator : IServiceLocator
+    {
+        private TestClass testClass;
 
-//        [Test]
-//        public void Returns_the_resolved_value_from_the_service_locator()
-//        {
-//            var expectedTestClass = new TestClass();
+        public void SetServiceLocatorToReturnThisWhenResolvingTestClass(TestClass testClass)
+        {
+            this.testClass = testClass;
+        }
 
-//            var serviceLocator = new TestServiceLocator();
-//            serviceLocator.SetServiceLocatorToReturnThisWhenResolvingTestClass(expectedTestClass);
+        #region Implementation of IDisposable
 
-//            var lazy = new Lazy<TestClass>(serviceLocator);
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
 
-//            lazy.Value.ShouldBeSameAs(expectedTestClass);
-//        }
-//    }
+        #endregion
 
-//    public class TestClass
-//    {
-//    }
+        #region Implementation of IServiceLocator
 
-//    public class TestServiceLocator : IServiceLocator
-//    {
-//        private TestClass testClass;
+        public T Resolve<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void SetServiceLocatorToReturnThisWhenResolvingTestClass(TestClass testClass)
-//        {
-//            this.testClass = testClass;
-//        }
+        public T Resolve<T>(string key) where T : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        #region Implementation of IDisposable
+        public T Resolve<T>(Type type) where T : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Dispose()
-//        {
-//            throw new NotImplementedException();
-//        }
+        public object Resolve(Type type)
+        {
+            if (type == typeof (TestClass))
+                return testClass;
+            return null;
+        }
 
-//        #endregion
+        public IList<T> ResolveServices<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        #region Implementation of IServiceLocator
+        public IServiceRegistrar Batch()
+        {
+            throw new NotImplementedException();
+        }
 
-//        public T Resolve<T>() where T : class
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void Register<Interface>(Type implType) where Interface : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        public T Resolve<T>(string key) where T : class
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void Register<Interface, Implementation>() where Implementation : class, Interface
+        {
+            throw new NotImplementedException();
+        }
 
-//        public T Resolve<T>(Type type) where T : class
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void Register<Interface, Implementation>(string key) where Implementation : class, Interface
+        {
+            throw new NotImplementedException();
+        }
 
-//        public object Resolve(Type type)
-//        {
-//            if (type == typeof(TestClass))
-//                return testClass;
-//            return null;
-//        }
+        public void Register(string key, Type type)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public IList<T> ResolveServices<T>() where T : class
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void Register(Type serviceType, Type implType)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public IServiceRegistrar Batch()
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void Register<Interface>(Interface instance) where Interface : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Register<Interface>(Type implType) where Interface : class
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void Release(object instance)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Register<Interface, Implementation>() where Implementation : class, Interface
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Register<Interface, Implementation>(string key) where Implementation : class, Interface
-//        {
-//            throw new NotImplementedException();
-//        }
+        public TService Inject<TService>(TService instance) where TService : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Register(string key, Type type)
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void TearDown<TService>(TService instance) where TService : class
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void Register(Type serviceType, Type implType)
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public void Register<Interface>(Interface instance) where Interface : class
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public void Release(object instance)
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public void Reset()
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public TService Inject<TService>(TService instance) where TService : class
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public void TearDown<TService>(TService instance) where TService : class
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        #endregion
-//    }
-//}
+        #endregion
+    }
+}
